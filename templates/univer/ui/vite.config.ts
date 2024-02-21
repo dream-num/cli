6c4f0103e7e4e435720b80b41bdad4e164073e98
白熱 <sonne@asaki.me>
2024-02-21 14:53:55 +0800

    feat: update template

--- templates/univer/ui/vite.config.ts
+++ templates/univer/ui/vite.config.ts
@@ -1,34 +1,7 @@
 import createViteConfig from '@univerjs/shared/vite';
 import pkg from './package.json';
 
-export default ({ mode }) => createViteConfig({
-    build: {
-        rollupOptions: {
-            external: [
-                '@univerjs/core',
-                '@univerjs/design',
-                '@univerjs/ui',
-                '@wendellhu/redi',
-                '@wendellhu/redi/react-bindings',
-                'clsx',
-                'react',
-                'rxjs'
-            ],
-            output: {
-                assetFileNames: 'index.css',
-                globals: {
-                    '@univerjs/core': 'UniverCore',
-                    '@univerjs/design': 'UniverDesign',
-                    '@wendellhu/redi': '@wendellhu/redi',
-                    '@wendellhu/redi/react-bindings': '@wendellhu/redi/react-bindings',
-                    clsx: 'clsx',
-                    react: 'React',
-                    rxjs: 'rxjs',
-                },
-            },
-        },
-    },
-}, {
+export default ({ mode }) => createViteConfig({}, {
     mode,
     pkg,
     features: {
