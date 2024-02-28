aff24fffdbf115135a47ef4b0a8b04d8ed6b32cb
白熱 <sonne@asaki.me>
2024-02-28 16:50:00 +0800

    feat: update templates

--- templates/normal/ui/demo/main.ts
+++ templates/normal/ui/demo/main.ts
@@ -8,6 +8,7 @@ import "@univerjs/sheets-formula/lib/index.css";
 import { Univer } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
 import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
 import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
 import { UniverSheetsPlugin } from "@univerjs/sheets";
@@ -36,6 +37,7 @@ univer.registerPlugin(UniverUIPlugin, {
 univer.registerPlugin(UniverDocsPlugin, {
   hasScroll: false,
 });
+univer.registerPlugin(UniverDocsUIPlugin)
 
 // sheet plugins
 univer.registerPlugin(UniverSheetsPlugin, {
